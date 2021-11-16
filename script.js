744b4dc2e5a025b75c09804aeb99ca09d9afe213
Wizdan10 <76710531+Wizdan10@users.noreply.github.com>
2021-11-16 18:45:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function login(){
+    var a = document.getElementById("password").value;
+    if (a == 1234) {
+        alert("Login Sukses!");
+    }else{
+        alert("Silahkan masukan password anda yang benar!")
+    }
+}
